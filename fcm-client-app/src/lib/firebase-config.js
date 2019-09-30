fe7aa4be9dfab488f4260e8137af103dcbabb9e7
Le Nho Thang <gianggiang1511@gmail.com>
2019-09-30 21:34:01 +0700

    first commit

---
+++ fcm-client-app/src/lib/firebase-config.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCxBy4UtyjalURK7tko1DrIB_TmJ_5I7OU",
+    authDomain: "thongbao-b2db4.firebaseapp.com",
+    databaseURL: "https://thongbao-b2db4.firebaseio.com",
+    projectId: "thongbao-b2db4",
+    storageBucket: "",
+    messagingSenderId: "462167424669",
+    appId: "1:462167424669:web:8c7396a940e909c3af5e74",
+    measurementId: "G-60CW8S6PK2"
+};
+
+export default firebase.initializeApp(firebaseConfig);
